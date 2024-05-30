3a41a3fb457b354eb9f1423e090ff394b2562b0c
serg-ht <sergiohteixeira@gmail.com>
2024-05-30 13:23:38 -0300

    Resumo das aulas de github

---
+++ GIThubAulas.py
@@ -0,0 +1,31 @@
+# no linux é possivel escrever os comandos do git direto no terminal
+# no windows tem que ser no terminal do git hub
+# duas autenticacoes antes de comecar:
+    # git config --global user.name 'serg-ht'
+    # git config --global user.email 'sergiohteixeira@gmail.com'
+
+#dois arquivos importantes:
+# 1 gitignore (configura o git para ignorar alguns arquivos dentro do repositorio)
+    ''' quando subir os arquivos no repositorio o git não vai subir as pastas que estao especificadas nesse arquivo
+    por exemplo pastas .env
+echo .env >> . gitignore
+    '''
+# a outra pasta importante é o readme.txt
+
+
+### AUL 6
+# git it add .
+# vai mandar os aqruivos para serem commitados
+
+# git commit manda tudo para o git hub
+ # git commit -m "primeiro commit"
+# git push. efetua
+
+#### aula 7 branchs
+ '''
+ quando se cria uma branch todo codigo é o mesmo até aquela ramificacao
+depois dela toda modificacao vai afetar apenas a branch e nao o codigo principal
+quando quiser juntar a branch na principal usa-se o comando merge
+
+'''
+
